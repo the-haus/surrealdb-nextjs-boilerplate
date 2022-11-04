a4e40270f0f98313ce159110678bc21cf9c7fd7b
Joost van Berkel <joost@the.haus>
2022-11-04 13:11:34 +0100

    Moved eslint executor to codegen

--- codegen.ts
+++ codegen.ts
@@ -3,19 +3,20 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
     schema: `${process.env.NEXT_PUBLIC_HTTP_LINK}/schema.graphql`,
     documents: ["./src/graphql/*.graphql"],
+    hooks: { afterOneFileWrite: ["eslint --fix"] },
     generates: {
         "./src/graphql/schema-server.graphql": {
             plugins: ["schema-ast"]
         },
         "./src/types/schema.d.ts": {
-            schema: ["./src/graphql/schema.graphql", "./src/graphql/operations.graphql"],
+            schema: ["./src/graphql/operations.graphql"],
             plugins: ["typescript", "typescript-operations"]
         },
-        "./src/types/fragments.tsx": {
+        "./src/hooks/fragments.tsx": {
             preset: "import-types",
             plugins: ["typescript-react-apollo"],
             presetConfig: {
-                typesPath: "./schema.d"
+                typesPath: "../types/schema.d"
             },
             config: {
                 withHOC: false,
